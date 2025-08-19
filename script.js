644c3676081c1b4bfbf4817dd7152d919547d282
sutirtha <80669786+sutirtha12@users.noreply.github.com>
2025-08-19 14:43:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
-  
+	 event.preventDefault()
+	const radius=document.getElementById("radius")
+	const volume=document.getElementById("volume")
+	const r=Number(radius.value)
+
+	if (isNaN(r) || r < 0) {
+    volume.value = "NaN";
+    return;
+  }
+	
+		volume.value=((4/3)*Math.PI*(r ** 3)).toFixed(4)
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+const button=document.getElementById("button")
+button.addEventListener("click",volume_sphere)
